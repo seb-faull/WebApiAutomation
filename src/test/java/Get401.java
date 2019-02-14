830de7f27d76576669b6dba880edb2334de45f53
bxxstian <seb_faull@hotmail.co.uk>
2019-02-14 17:21:21 +0000

    Refactor and include dataproviders

--- src/test/java/Get401.java
+++ src/test/java/Get401.java
@@ -6,6 +6,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -28,10 +29,19 @@ public class Get401 extends BaseClass {
         // response.close();
     }
 
-    @Test
-    public void userReturns401() throws IOException {
+    @DataProvider
+    private Object[][] endpoints() {
+        return new Object[][]{
+                {"/user"},
+                {"/user/followers"},
+                {"/notifications"},
+        };
+    }
 
-        HttpGet get = new HttpGet(BASE_ENDPOINT + "/user");
+    @Test(dataProvider = "endpoints")
+    public void userReturns401(String endpoint) throws IOException {
+
+        HttpGet get = new HttpGet(BASE_ENDPOINT + endpoint);
 
         HttpResponse response = client.execute(get);
 
@@ -41,29 +51,4 @@ public class Get401 extends BaseClass {
 
     }
 
-    @Test
-    public void userFollowersReturns401() throws IOException {
-
-        HttpGet get = new HttpGet(BASE_ENDPOINT + "/user/followers");
-
-        HttpResponse response = client.execute(get);
-
-        int getStatusCode = response.getStatusLine().getStatusCode();
-
-        assertEquals(getStatusCode, 401);
-
-    }
-
-    @Test
-    public void notificationsReturns401() throws IOException {
-
-        HttpGet get = new HttpGet(BASE_ENDPOINT + "/notifications");
-
-        HttpResponse response = client.execute(get);
-
-        int getStatusCode = response.getStatusLine().getStatusCode();
-
-        assertEquals(getStatusCode, 401);
-
-    }
 }
